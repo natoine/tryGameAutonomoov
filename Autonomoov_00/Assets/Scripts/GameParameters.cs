47dcef82bd8652ca47333485203de94fb72449c9
Marie Legrand <marie.legrand@naturalpad.fr>
2019-01-23 11:46:47 +0100

    XML parsing for parameters reading

---
+++ Autonomoov_00/Assets/Scripts/GameParameters.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MOVEMENT
+{
+    R_HAND,
+    L_HAND
+}
+
+public class GameParameters : MonoBehaviour
+{
+    public static GameParameters instance;
+    private int _playerCount = 0;
+    private int _timerInSeconds = 0;
+    private MOVEMENT _requiredMovement = MOVEMENT.L_HAND;
+
+    void Awake()
+    {
+        Debug.Log("Coucou params");
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public int GetPlayerCount()
+    {
+        return _playerCount;
+    }
+    public void SetPlayerCount(int count)
+    {
+        _playerCount = count;
+    }
+
+    public int GetTimer()
+    {
+        return _timerInSeconds;
+    }
+    public void SetTimer(int timer)
+    {
+        _timerInSeconds = timer;
+    }
+
+    public MOVEMENT GetMovement()
+    {
+        return _requiredMovement;
+    }
+    public void SetMovement(MOVEMENT movement)
+    {
+        _requiredMovement = movement;
+    }
+}
---
+++ Autonomoov_00/Assets/Scripts/XMLLoader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using UnityEngine;
+
+public class XMLLoader : MonoBehaviour
+{
+    /* <summary>
+     * Ce singleton charge le fichier XML des paramètres. 
+     * </summary> */
+    public static XMLLoader instance = null;
+    private TextAsset xmlFile;
+    private XmlDocument xmlDocument = new XmlDocument();
+
+    void Start()
+    {
+        if (instance)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            Initiate(Application.systemLanguage);
+        }
+    }
+
+    public bool Initiate(SystemLanguage language)
+    {
+        xmlFile = Resources.Load<TextAsset>("Parameters");
+        if (xmlFile)
+        {
+            Debug.Log(xmlFile.text);
+            xmlDocument.LoadXml((xmlFile.text));
+            foreach (XmlNode node in xmlDocument.ChildNodes[1])
+            {
+                switch (node.Name) {
+                    case "playersCount":
+                        {
+                        GameParameters.instance.SetPlayerCount(int.Parse(node.InnerText));
+                        break;
+                    }
+                    case "timer":
+                        {
+                            GameParameters.instance.SetTimer(int.Parse(node.InnerText));
+                            break;
+                        }
+                    case "movement":
+                        {
+                            if(node.InnerText == "L_HAND")
+                            {
+                                GameParameters.instance.SetMovement(MOVEMENT.L_HAND);
+                            }
+                            if (node.InnerText == "R_HAND")
+                            {
+                                GameParameters.instance.SetMovement(MOVEMENT.R_HAND);
+                            }
+                            break;
+                        }
+                }
+            }
+            Debug.Log("Registred player count = " + GameParameters.instance.GetPlayerCount());
+            Debug.Log("Registred timer in seconds = " + GameParameters.instance.GetTimer());
+            Debug.Log("Registred movement = " + GameParameters.instance.GetMovement().ToString());
+
+            return true;
+        }
+        return false;
+    }
+
+    public XmlDocument GetXmlDocument()
+    {
+        return xmlDocument;
+    }
+}
